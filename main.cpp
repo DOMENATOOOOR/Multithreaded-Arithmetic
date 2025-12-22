171fc7cbe3a25a39d786581ddac3db1e525e98a9
Pan_Sasha <pankov.aleksander05@gmail.com>
2025-12-23 00:40:33 +0700

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,8 +1,6 @@
-#include "LoggerAndFileTask.h"
-
-static double total_sum = 0.0;
-
-
+#include "Logger.h"
+#include "TaskProcessor.h"
+#include <string>
 
 int main() {
     Logger logger("LoggerOfArithmetic.txt");
@@ -10,22 +8,7 @@ int main() {
     std::string input_dir = "/Users/mac_aleksandr/data";
     std::string output_file = "OutputOfArithmetic.txt";
 
-    std::vector<std::future<double>> futures;
-    for (const auto& p : std::filesystem::directory_iterator(input_dir)) {
-        if(p.path().filename().string().starts_with("in_")) {
-            FileTask task(p.path(), logger);
-            futures.push_back(std::async(std::launch::async, task));
-        }
-    }
-
-    for (auto& f : futures) {
-        total_sum += f.get();
-    }
-
-    std::ofstream out(output_file, std::ios::out | std::ios::app);
-    out << total_sum << std::endl;
-    logger.log("Total sum in file: " + output_file + '\n' +  "Done!" + '\n' + '\n' );
-    out.close();
+    processFiles(input_dir, output_file, logger);
 
     return 0;
 }
